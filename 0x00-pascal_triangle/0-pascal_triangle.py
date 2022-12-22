6e2daf0df0a990ff07d2efc7336b927a401e1c71
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-12-22 22:04:36 +0100

    [Fix]: Correcting all errors according to standards

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
 
-""" 
-	Script that demonstrates the pascal's triangle
+"""
+Script that demonstrates the pascal's triangle
 """
 
+
 def pascal_triangle(n):
-	"""
-		Function for creating the pascal's triangle
+    """
+        Function for creating the pascal's triangle
 
-		Arguments:
-			n: Integer variable in the triangle
+        Arguments:
+            n: Integer variable in the triangle
 
-		Return:
-			List if n > 0
-			empty list if n <= 0
-	"""
-	i, other = 0, []
-	while i < n:
-		y, tri = 0, []
-		while y <= i:
-			tri.append(combination(i, y))
-			y += 1
-		other.append(tri)
-		i += 1
-	return other
-	
+        Return:
+            List if n > 0
+            empty list if n <= 0
+    """
+    i, other = 0, []
+    while i < n:
+        y, tri = 0, []
+        while y <= i:
+            tri.append(combination(i, y))
+            y += 1
+        other.append(tri)
+        i += 1
+    return other
 
-def combination(x:int, y:int) -> int:
-	"""
-		Function used for recreating the combination
 
-		Arguments:
-			x: First integer
-			y: Second integer
+def combination(x: int, y: int) -> int:
+    """
+        Function used for recreating the combination
 
-		Return:
-			Combination of the integer
-	"""
-	i, j, k = 1, 1, 1
-	for w in range(1, x+1):
-		i *= w
-	for w in range(1, (x - y) + 1):
-		j *= w
-	for w in range(1, y+1):
-		k *= w
-	ans = (i)/(j * k)
-	return int(ans)
+        Arguments:
+            x: First integer
+            y: Second integer
+
+        Return:
+            Combination of the integer
+    """
+    i, j, k = 1, 1, 1
+    for w in range(1, x+1):
+        i *= w
+    for w in range(1, (x - y) + 1):
+        j *= w
+    for w in range(1, y+1):
+        k *= w
+    ans = (i)/(j * k)
+    return int(ans)
