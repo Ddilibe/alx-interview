4e482bfe5b88e13adc221b27ef47c80e7c4b8177
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-12-22 21:55:02 +0100

    [Fix]: Rewrote the functions in paschal script

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,10 +4,6 @@
 	Script that demonstrates the pascal's triangle
 """
 
-import math
-
-light = [1]
-
 def pascal_triangle(n):
 	"""
 		Function for creating the pascal's triangle
@@ -23,9 +19,30 @@ def pascal_triangle(n):
 	while i < n:
 		y, tri = 0, []
 		while y <= i:
-			tri.append(math.comb(i, y))
+			tri.append(combination(i, y))
 			y += 1
 		other.append(tri)
 		i += 1
 	return other
 	
+
+def combination(x:int, y:int) -> int:
+	"""
+		Function used for recreating the combination
+
+		Arguments:
+			x: First integer
+			y: Second integer
+
+		Return:
+			Combination of the integer
+	"""
+	i, j, k = 1, 1, 1
+	for w in range(1, x+1):
+		i *= w
+	for w in range(1, (x - y) + 1):
+		j *= w
+	for w in range(1, y+1):
+		k *= w
+	ans = (i)/(j * k)
+	return int(ans)
