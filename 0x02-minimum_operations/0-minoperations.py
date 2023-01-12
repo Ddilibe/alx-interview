e0981bf1c19c0d07854328580211eb1fd8694524
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-12 21:54:30 +0100

    [Chore]: Trying to complete minimum operation tasks

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
 """ Script that tests and visualizes minimum operations """
 
+
 def minOperations(n: int) -> int:
-	if n == 2:
-		return 1
-	if n == 1:
-		return 0
-	i, h = 2, "H"
-	while True:
-		if n % i == 0:
-			break
-		i += 1
-	first = int(n/i) + i
-	i, h = 3, "H"
-	while True:
-		if n % i == 0:
-			break
-		i += 1
-	second = int(n/i) + i
-	if first < second:
-		return first
-	return second
+    """
+        Function to determine the minimum operation over the duplication of a
+        certain string
+
+        Args:
+            :param n [int] - The first argument
+
+        Return:
+            This function returns an integer as a value
+    """
+    if n == 2:
+        return 1
+    if n == 1:
+        return 0
+    i, h = 2, "H"
+    while True:
+        if n % i == 0:
+            break
+        i += 1
+    first = int(n/i) + i
+    i, h = 3, "H"
+    while True:
+        if n % i == 0:
+            break
+        i += 1
+    second = int(n/i) + i
+    if first < second:
+        return first
+    return second
