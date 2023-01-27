4d43f8c82d9e724efc4b06ba950bb6ef5a0019e9
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-27 03:44:11 +0100

    [Chore]: Done with this task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
-""" Script that validate UTF-8 encoding """
+"""
+Define validUTF8(data) function that validates whether a
+string of ints represents a valid UTF-8 encoding.
+"""
 from itertools import takewhile
 
 
-def validUTF8(data: List[int]) -> bool:
-	"""
-		Function thst determines if a given data set
-		represents valid UTF-8 encoding
+def int_to_bits(nums):
+    """
+    Helper function
+    Convert ints to bits
+    """
+    for num in nums:
+        bits = []
+        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
+        while mask:
+            mask >>= 1
+            bits.append(bool(num & mask))
+        yield bits
 
-		Args:
-			:params @data [List[int]] - The first argument contains
-			a list of ints that will be validated
 
-		Return:
-			The return value is a boolean
-	"""
+def validUTF8(data):
+    """
+    Takes a list of ints and returns true if the list is
+    a valid UTF-8 encoding, else returns false
+    Args:
+        data : List of ints representing possible UTF-8 encoding
+    Return:
+        bool : True or False
+    """
     bits = int_to_bits(data)
     for byte in bits:
         # if single byte char, then valid. continue
@@ -36,17 +50,3 @@ def validUTF8(data: List[int]) -> bool:
             if byte[0:2] != [1, 0]:
                 return False
     return True
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
