552c7e423e59aa4f88b1c64fae31840aa227ad71
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-27 03:41:11 +0100

    [Chore]: Done with this task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Script that validate UTF-8 encoding """
-from typing import List
+from itertools import takewhile
 
 
 def validUTF8(data: List[int]) -> bool:
@@ -15,16 +15,38 @@ def validUTF8(data: List[int]) -> bool:
 		Return:
 			The return value is a boolean
 	"""
-	bin_data = [str(bin(i)).split('0b')[1] for i in data]
-	check = True
-	for i in bin_data:
-		j = 0
-		for w in i:
-			if w == '1':
-				j +=1
-			else:
-				break
-		if j > 4:
-			check = False
-			break
-	return check
+    bits = int_to_bits(data)
+    for byte in bits:
+        # if single byte char, then valid. continue
+        if byte[0] == 0:
+            continue
+
+        # if here, byte is multi-byte char
+        ones = sum(takewhile(bool, byte))
+        if ones <= 1:
+            return False
+        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
+            return False
+
+        for _ in range(ones - 1):
+            try:
+                byte = next(bits)
+            except StopIteration:
+                return False
+            if byte[0:2] != [1, 0]:
+                return False
+    return True
+
+
+def int_to_bits(nums):
+    """
+    Helper function
+    Convert ints to bits
+    """
+    for num in nums:
+        bits = []
+        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
+        while mask:
+            mask >>= 1
+            bits.append(bool(num & mask))
+        yield bits
