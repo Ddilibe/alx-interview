a4cbd7d2c0a4d57aa7f946bd18382bf991ce4b8c
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-02-18 12:56:22 +0100

    [Chore]: Done with the rotate matrix 90 degrees task

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Script that rotates a matrix at 90 degrees clockwise """
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+    """
+        Function for rotating a matrix 90 degress clockwise
+        Args:
+            :params: @matrix - A list of list containing the
+            matrix to rotate
+        Return:
+            No return
+    """
+    old_matrix, n = matrix.copy(), len(matrix)
+    for i in range(n):
+        new_list = []
+        for j in range(n):
+            new_list.append(old_matrix[j][i])
+        new_list.reverse()
+        matrix[i] = new_list
